465bb7fb3d8ea675f50fc25a39222562ceaea3c8
lukeinator42 <lukasgrasse@gmail.com>
2023-11-03 17:24:11 -0600

    wav example working

--- server_recognize_wav.py
+++ server_recognize_wav.py
@@ -1,8 +1,10 @@
 import json
+import time
 import wave
 import rero_grpc.audio_pb2 as audio
 import rero_grpc.speech_recognition_pb2_grpc as sr_grpc
 import grpc
+import numpy as np
 
 class AudioStreamer:
     def __init__(self, wav_filename, buffer_size=1024):
@@ -11,10 +13,12 @@ class AudioStreamer:
         self.wav_file = None
         self.is_end_of_stream = False
 
+    #open wav file
     def open(self):
         self.wav_file = wave.open(self.wav_filename, 'rb')
         self.is_end_of_stream = False
 
+    #close wav file
     def close(self):
         if self.wav_file:
             self.wav_file.close()
@@ -29,10 +33,8 @@ class AudioStreamer:
         # Read and yield the raw audio data in chunks.
         raw_data = self.wav_file.readframes(self.frames_per_buffer)
 
-
         if not raw_data:
-            self.is_end_of_stream = True
-            raise StopIteration
+            raw_data = np.zeros(self.frames_per_buffer, dtype=np.int16).tobytes()
 
         # Create an Audio message
         audio_message = audio.Audio(
@@ -43,6 +45,8 @@ class AudioStreamer:
             sample_rate=self.sample_rate,
             raw_data=raw_data
         )
+
+        time.sleep(0.064)
 
         return audio_message
 
@@ -59,31 +63,39 @@ class AudioStreamer:
         # Initialize frames_per_buffer
         self.frames_per_buffer = self.buffer_size // (self.bytes_per_sample * self.num_channels)
 
+    #basically useless as silence is streamed afterwards
     def has_leftover(self):
         return not self.is_end_of_stream
 
 # Usage example:
 def recognize_audio(streamer, stub):
+    #start streamer
     streamer.initialize()
 
+    # keep looping from where we left off
     while streamer.has_leftover():
 
+        #recognize some text
         response = stub.RecognizeSpeech(streamer)
 
         #parse json result
         parsed_result = json.loads(response.result)
 
+        # if we get silence as a result we are done
+        if 'text' not in parsed_result or parsed_result['text'] == '':
+            break
+
         #print result
         print("Speech Recognition Result: ", parsed_result['text'])
 
-        streamer.close()
+    streamer.close()
 
 # Create a gRPC channel and stub
-channel = grpc.insecure_channel('0.0.0.0:50052')
-stub = sr_grpc.SpeechRecognitionStub(channel)
+with grpc.insecure_channel('0.0.0.0:50052') as channel:
+    stub = sr_grpc.SpeechRecognitionStub(channel)
 
-# Create an AudioStreamer instance
-streamer = AudioStreamer('/home/lukas/Desktop/jfk2.wav')
+    # Create an AudioStreamer instance
+    streamer = AudioStreamer('/home/lukas/Desktop/jfk2.wav')
 
-# Start the recognition process
-recognize_audio(streamer, stub)
+    # Start the recognition process
+    recognize_audio(streamer, stub)
